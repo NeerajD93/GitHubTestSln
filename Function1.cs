8709e2650e46282a8a099a90b9a0ea41dd7ecc64
Neeraja Dachuri <820430@cognizant.com>
2019-12-18 17:47:43 +0530

    check Azure Pipelines

--- Function1.cs
+++ Function1.cs
@@ -10,7 +10,7 @@ namespace GitHubTestSln
 {
     public static class Function1
     {
-        [FunctionName("TestFunction")]
+        [FunctionName("TestFunctionFn")]
         public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
         {
              log.Info("C# HTTP trigger function processed a request.");
